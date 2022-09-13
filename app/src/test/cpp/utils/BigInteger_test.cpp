0b91d17b8633b9cc6e6b171212846a445a7557ac
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2022-09-13 12:54:01 +0800

    karatsuba test prepare void

--- app/src/test/cpp/utils/BigInteger_test.cpp
+++ app/src/test/cpp/utils/BigInteger_test.cpp
@@ -10,13 +10,8 @@ int main(int argc, char *argv[])
     auto restart = std::chrono::high_resolution_clock::now();
     
     std::cout << "static karatsuba test \n";
-    //std::vector<word> res1(8, 0);
-    const std::vector<word> resA{1002387, 90719736, 145201,0};
-    const std::vector<word> resB{9021, 14326711 ,0, 0};
-    //karatsuba(res1.data(), resA.data(), resB.data(), 4);
-    std::cout << "correct "<< (BigInteger(resA) * BigInteger(resB)) << std::endl;
-    //std::cout << "function test " << BigInteger(res1) << std::endl;
-    
+    karatsuba_test();
+
     std::cout << "BigInteger code test \n";
 	BigInteger a, b;
     //comparation test
