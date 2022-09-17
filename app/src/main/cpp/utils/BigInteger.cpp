a50bfcf8e8a4dc058847c12080a6f85e4dd504a8
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-09-17 21:27:03 +0800

    test3

--- app/src/main/cpp/utils/BigInteger.cpp
+++ app/src/main/cpp/utils/BigInteger.cpp
@@ -215,7 +215,7 @@ BigInteger &BigInteger::operator--()
     }
     else
     {
-        for (size_t i = 0; i < n && carry0; i++)
+        for (size_t i = 0; i < n && carry; i++)
             carry0 = this->words[i] < (this->words[i] -= carry);
         while (words.size() && !words.back())
             words.pop_back();
