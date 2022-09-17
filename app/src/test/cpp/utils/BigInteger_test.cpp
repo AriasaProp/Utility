0fe92133be9d1a0e594c498b9c59b630c41b1fcb
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-09-17 23:20:16 +0800

    test15

--- app/src/test/cpp/utils/BigInteger_test.cpp
+++ app/src/test/cpp/utils/BigInteger_test.cpp
@@ -179,8 +179,8 @@ int main(int argc, char * argv[]) {
     passed &= result;
     if (!result)
         std::cout << "    '<<=' operator error \n";
-    return 0;
     a = BigInteger("1152921504606846976\0"), b = 2;
+    return 0;
     // "^" operator test
     result = ((b ^ 60) == a);
     passed &= result;
