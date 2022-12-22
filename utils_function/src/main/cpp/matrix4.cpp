5862bb31e864bbb9aa99f28b31a8a1abe156e975
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-12-22 16:29:44 +0800

    fix error

--- utils_function/src/main/cpp/matrix4.cpp
+++ utils_function/src/main/cpp/matrix4.cpp
@@ -4,7 +4,7 @@
 static const unsigned int MATRIX_SIZE = sizeof(float) * 16;
 //matrix4 constructors
 matrix4::matrix4() {
-	memcpy(values, [16]float{1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1}, MATRIX_SIZE);
+	memcpy(values, (float[]){1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1}, MATRIX_SIZE);
 }
 matrix4::matrix4(float v[16]) {
 	memcpy(values, v, MATRIX_SIZE);
