9a5d1376b5fb83f96bdaf2998d174c5f447a22b9
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-12-22 18:04:03 +0800

    fix test 5

--- utils_function/src/test/cpp/big_decimal_test.cpp
+++ utils_function/src/test/cpp/big_decimal_test.cpp
@@ -3,16 +3,18 @@
 bool big_decimal_test()
 {
     std::cout << "big_decimal test begin! " << std::endl;
-    
-    big_decimal x = 1.333333f;
-    big_decimal y = 3.145928f;
-    big_decimal z = 7.0;
-    z /= 3;
-    
-    std::cout << x << std::endl;
-    std::cout << y << std::endl;
-    std::cout << z << std::endl;
-    
+    try {
+	    big_decimal x = 1.333333f;
+	    big_decimal y = 3.145928f;
+	    big_decimal z = 7.0;
+	    z /= 3;
+	    
+	    std::cout << x << std::endl;
+	    std::cout << y << std::endl;
+	    std::cout << z << std::endl;
+    } catch (const char *er) {
+    	std::cout << er << std::endl;
+    }
     std::cout << "big_decimal test end! " << std::endl;
     return true;
 }
