3486b852151cead314371f7796c1b2f6c0c4717e
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-12-22 17:29:44 +0800

    fix test 7

--- utils_function/src/test/cpp/app_test.cpp
+++ utils_function/src/test/cpp/app_test.cpp
@@ -7,8 +7,8 @@ int main(int argv, char *args[])
 {
     bool passed = true;
     
-    //passed &= BigInteger_test();
-    //passed &= big_decimal_test();
+    passed &= BigInteger_test();
+    passed &= big_decimal_test();
     passed &= matrix4_test();
     
     return !passed;
