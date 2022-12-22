3f293e8368d41162500e04b26547e34d5957473c
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-12-22 17:38:16 +0800

    fix test

--- utils_function/src/test/cpp/big_decimal_test.cpp
+++ utils_function/src/test/cpp/big_decimal_test.cpp
@@ -7,7 +7,7 @@ bool big_decimal_test()
     big_decimal x = 1.333333;
     big_decimal y = 3.145928;
     big_decimal z = 7.0;
-    z /= 3;
+    //z /= 3;
     
     std::cout << x << std::endl;
     std::cout << y << std::endl;
