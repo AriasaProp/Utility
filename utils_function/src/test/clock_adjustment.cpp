161e7ac577b0fa91a7e80e981925ddcffa5385cd
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-08-24 10:43:52 +0800

    test clocking

--- utils_function/src/test/clock_adjustment.cpp
+++ utils_function/src/test/clock_adjustment.cpp
@@ -13,40 +13,40 @@ struct clock_adjustment::private_data {
 
 clock_adjustment::clock_adjustment(const char *l) {
   data = new private_data;
-  data.label = new char[strlen(l)];
-  strcpy(data.label, l);
-  data.safe_time_part = data.safe_time = std::chrono::steady_clock::now();
-  std::cout << "Clocking for " << data.label << " started now!" << std::endl;
+  data->label = new char[strlen(l)];
+  strcpy(data->label, l);
+  data->safe_time_part = data->safe_time = std::chrono::steady_clock::now();
+  std::cout << "Clocking for " << data->label << " started now!" << std::endl;
 }
 unsigned long clock_adjustment::get_clock(const period p) {
-  data.temp_time = std::chrono::steady_clock::now();
+  data->temp_time = std::chrono::steady_clock::now();
   unsigned long res = 0;
   switch (p) {
     case hours:
-      res = std::chrono::duration_cast<std::chrono::hours>(data.temp_time - data.safe_time_part).count();
+      res = std::chrono::duration_cast<std::chrono::hours>(data->temp_time - data->safe_time_part).count();
       break;
     case minutes:
-      res = std::chrono::duration_cast<std::chrono::minutes>(data.temp_time - data.safe_time_part).count();
+      res = std::chrono::duration_cast<std::chrono::minutes>(data->temp_time - data->safe_time_part).count();
       break;
     case seconds:
-      res = std::chrono::duration_cast<std::chrono::seconds>(data.temp_time - data.safe_time_part).count();
+      res = std::chrono::duration_cast<std::chrono::seconds>(data->temp_time - data->safe_time_part).count();
       break;
     case milliseconds:
-      res = std::chrono::duration_cast<std::chrono::milliseconds>(data.temp_time - data.safe_time_part).count();
+      res = std::chrono::duration_cast<std::chrono::milliseconds>(data->temp_time - data->safe_time_part).count();
       break;
     default:
     case microseconds:
-      res = std::chrono::duration_cast<std::chrono::microseconds>(data.temp_time - data.safe_time_part).count();
+      res = std::chrono::duration_cast<std::chrono::microseconds>(data->temp_time - data->safe_time_part).count();
       break;
   }
-  data.safe_time_part = std::chrono::steady_clock::now();
+  data->safe_time_part = std::chrono::steady_clock::now();
   return res;
 }
 clock_adjustment::~clock_adjustment() {
-  data.temp_time = std::chrono::steady_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(data.temp_time - data.safe_time);
+  data->temp_time = std::chrono::steady_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(data->temp_time - data->safe_time);
     
-  std::cout << "Clocking for " << data.label << " Ended in ";
+  std::cout << "Clocking for " << data->label << " Ended in ";
   if (duration.count() > 3600000000) {  // Jika lebih dari 1 jam
       std::cout << duration.count() / 3600000000 << " hours" << std::endl;
   } else if (duration.count() > 60000000) {  // Jika lebih dari 1 menit
@@ -58,6 +58,6 @@ clock_adjustment::~clock_adjustment() {
   } else {  // Mikrodetik
       std::cout << duration.count() << " microseconds" << std::endl;
   }
-  delete[] data.label;
+  delete[] data->label;
   delete data;
 }
