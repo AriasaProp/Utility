52dde35b063611075a62330f66f0f825f7fa0f12
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-08-25 16:02:58 +0800

    update tools

--- utils_function/src/test/matrix_test.cpp
+++ utils_function/src/test/matrix_test.cpp
@@ -5,7 +5,7 @@ bool matrix_test() {
   matrix<2, 2> matrix2x2(data2x2);
   matrix2x2.print();
 
-  const float data3x3[2][3] = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
+  const float data3x3[3][3] = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
   matrix<3, 3> matrix3x3(data3x3);
   matrix3x3.print();
   return true;
