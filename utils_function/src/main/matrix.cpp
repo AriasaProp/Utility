20cc26b0eba0f08cfff69fd35a27d7e227181712
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-08-25 09:11:03 +0800

    update tools

---
+++ utils_function/src/main/matrix.cpp
@@ -0,0 +1,72 @@
+#include "matrix.hpp"
+
+#define MATF(R,F) \
+template <unsigned H, unsigned V>\
+R matrix<H,V>::##F
+
+MATF(, matrix)() {}
+MATF(, ~matrix)() {}
+
+MATF(float, &operator[])(unsigned l) { return data[l/V][l % H]; }
+MATF(float, &operator[])(unsigned v, unsigned h) { return data[v][h]; }
+
+MATF(matrix, operator+) (const matrix& other) {
+    matrix result;
+    for (unsigned i = 0; i < H * V; ++i) {
+        result.data[i] = this->data[i] + other.data[i];
+    }
+    return result;
+}
+MATF(matrix, operator*)(const matrix& other) {
+    matrix result;
+    for (unsigned i = 0, j = 0, k = 0; i < H; ++i) {
+        for (j = 0; j < V; ++j) {
+            for (k = 0; k < V; ++k) {
+                result(i, j) += this->data[i * V + k] * other.data[k * V + j];
+            }
+        }
+    }
+    return result;
+}
+
+MATF(void, printInfo) () {
+    std::cout << std::endl;
+    for (unsigned i = 0, j = 0; i < V; i++) {
+        std::cout << "\n\[";
+        for (j = 0; j < H; j++) {
+            std::cout << "| " << data[v * H + h] << " |";
+        }
+        std::cout << "\]";
+    }
+    std::cout << std::endl;
+}
+
+MATF(size_t, number_of_digits) (double n) {
+	std::ostringstream strs;
+
+	strs << n;
+	return strs.str().size();
+}
+
+MATF(void, print_matrix) () {
+	size_t max_len_each_vert[nmax];
+	//find length each vertical
+	for (size_t j = 0; j < H; ++j) { //horizontal
+		size_t max_len {};
+
+		for (size_t i = 0; i < V; ++i) { //vertical
+			if (const auto num_length {number_of_digits(this->data[i][j])}; num_length > max_len)
+				max_len = num_length;
+		}
+
+		max_len_each_vert[j] = max_len;
+	}
+
+	for (size_t i = 0, j = 0; i < V; ++i) {
+		for (size_t j = 0; j < H; ++j) {
+			std::cout << (j == 0 ? "\n| " : "") << std::setw(max_len_each_vert[j]) << this->data[i][j] << (j == H - 1 ? " |" : " ");
+    }
+	}
+	std::cout << '\n';
+}
+
---
+++ utils_function/src/main/matrix.hpp
@@ -0,0 +1,38 @@
+#ifndef _MATRIX_INCLUDED_
+#define _MATRIX_INCLUDED_
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+/*
+   Horizontal ->
+Vertical  
+ |       a    b    c    d ....
+ v       aa   ab   ac   ad .....
+         ba   bb   bc   bd .....
+       .... ..... .... ... .....
+       ...a  ..b  ..c  ..d .....
+*/
+
+template <unsigned H, unsigned V>
+struct matrix {
+private:
+    float data[V][H] {};
+public:
+    matrix();
+    ~matrix();
+
+    float& operator[](unsigned);
+    float& operator[](unsigned, unsigned);
+    matrix operator+(const matrix&);
+    matrix operator*(const matrix&);
+    
+    void printInfo();
+
+    size_t number_of_digits(float);
+    void print_matrix(const float M[nmax][nmax], size_t n, size_t m);
+};
+
+
+#endif //_MATRIX_INCLUDED_
