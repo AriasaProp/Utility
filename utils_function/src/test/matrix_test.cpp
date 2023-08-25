95b6f9925d8e1a82e0946719aacb9cd49af7486c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-08-25 10:25:53 +0800

    update tools

--- utils_function/src/test/matrix_test.cpp
+++ utils_function/src/test/matrix_test.cpp
@@ -2,8 +2,11 @@
 
 bool matrix_test() {
   
-  matrix<2,2> mx2a{{12.f,17.f},{13.f,11.f}};
-  matrix<2,2> mx2b{{7.f,4.f},{18.f,0.f}};
+  
+  float a[2][2] {{12.f,17.f},{13.f,11.f}};
+  float b[2][2] {{7.f,4.f},{18.f,0.f}};
+  matrix<2,2> mx2a(a);
+  matrix<2,2> mx2b(b);
   
   mx2a.print_matrix();
   mx2b.print_matrix();
