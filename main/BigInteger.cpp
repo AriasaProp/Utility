151f7f789026ee469f346948f99afb61017255d3
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-12 08:45:40 +0800

    fix test

--- main/BigInteger.cpp
+++ main/BigInteger.cpp
@@ -137,7 +137,8 @@ BigInteger::operator double() const {
 char *BigInteger::to_chars() const {
   std::vector<word> A = words;
   const size_t texN = static_cast<size_t>(std::ceil(std::log10(std::pow(WORD_MASK,A.size())))) + 1;
-  char *text = new char[texN+1]{0};
+  char *text = new char[texN+1];
+  memset(text, ' ', texN+1);
   char *tcr = text + texN;
   while (A.size()) {
     word rmr = 0;
@@ -930,7 +931,8 @@ BigInteger BigInteger::operator<< (size_t n_bits) const {
 std::ostream &operator<<(std::ostream &out, const BigInteger &num) {
   std::vector<word> A = num.words;
   const size_t texN = static_cast<size_t>(std::ceil(std::log10(std::pow(WORD_MASK,A.size()))));
-  char *text = new char[texN+1]{" "};
+  char *text = new char[texN+1];
+  memset(text, ' ', texN+1);
   char *tcr = text + texN;
   while (A.size()) {
     word rmr = 0;
