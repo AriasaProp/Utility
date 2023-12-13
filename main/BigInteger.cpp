8a1dc74e7be180d5479351a7e7c39beedaba51d9
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-13 10:57:33 +0800

    fix BigInteger precision

--- main/BigInteger.cpp
+++ main/BigInteger.cpp
@@ -139,12 +139,13 @@ char *BigInteger::to_chars() const {
   if (A.empty()) {
     return new char[1]{'0'};
 	} else {
-	  size_t texN = static_cast<size_t>(std::ceil(std::log10(std::pow(WORD_MASK,A.size()-1) * A.back()) + 0.01));
+	  size_t texN = static_cast<size_t>(std::ceil(std::log10(std::pow(WORD_MASK,A.size()-1) * A.back()) + 0.01)) + 1;
 	  if (neg) ++texN;
 	  char *text = new char[texN+1];
 	  memset(text, ' ', texN+1);
 	  if (neg) *text = '-';
 	  char *tcr = text + texN;
+    *(tcr--) = '\0';
 	  while (!A.empty()) {
 	    word rmr = 0;
 	    for (std::vector<word>::reverse_iterator cur = A.rbegin(); cur != A.rend(); ++cur) {
