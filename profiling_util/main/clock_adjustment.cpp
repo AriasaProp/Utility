f204d2c0ba029f87940bfceaed4b55d325173633
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-05-26 13:06:19 +0800

    fixing in 26/05/2024

--- profiling_util/main/clock_adjustment.cpp
+++ profiling_util/main/clock_adjustment.cpp
@@ -67,6 +67,7 @@ profiling::clock_adjustment::~clock_adjustment() {
   if (dc) {  // micro sec
       std::cout << dc << " us ";
   }
+  
   std::cout << std::endl;
   delete[] data->label;
   delete data;
