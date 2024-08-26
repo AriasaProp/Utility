060985cc961bd818148d7d8049a125c1574d3df1
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-08-26 15:49:18 +0800

    fix : 08/26/24 15:49:18

--- math_util/main/random.cpp
+++ math_util/main/random.cpp
@@ -1,10 +1,12 @@
 #include <random>
 #include <atomic>
+#include <limits>
 
 #include "random.hpp"
 
 uint32_t random_uint32() {
-	static std::atomic<std::mt19937> rng(std::random_device{}());
-	static std::uniform_int_distribution<uint32_t> dist;
+  static std::atomic<std::mt19937> rng(std::random_device{}());
+  static std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
   return dist(rng);
 }
+
