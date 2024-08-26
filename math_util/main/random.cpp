20347d6d652506271c1db0da5fe02404fe2fa768
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-08-26 16:28:18 +0800

    fix3 : 08/26/24 16:28:18

--- math_util/main/random.cpp
+++ math_util/main/random.cpp
@@ -4,12 +4,10 @@
 
 #include "random.hpp"
 
-static std::atomic<uint32_t> seed(std::random_device{}());
+// Thread-local random number generator
+static thread_local std::mt19937 rng(std::random_device{}());
 static const std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
 
 uint32_t random_uint32() {
-  uint32_t n = dist(std::mt19937(seed.load()));
-  seed.store(n);
-  return n;
+  return dist(rng);
 }
-
