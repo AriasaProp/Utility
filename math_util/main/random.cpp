caac600821fcf1273fa88b78501da1862716d5fc
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-08-26 16:16:21 +0800

    fix12 : 08/26/24 16:16:21

--- math_util/main/random.cpp
+++ math_util/main/random.cpp
@@ -4,12 +4,11 @@
 
 #include "random.hpp"
 
-static std:atomic<uint32_t> seed(std::random_device{}());
+static std::atomic<uint32_t> seed(std::random_device{}());
 static const std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
 
 uint32_t random_uint32() {
-	std::mt19937 rng(seed.load());
-  uint32_t n = dist(rng);
+  uint32_t n = dist(std::mt19937(seed.load()));
   seed.store(n);
   return n;
 }
