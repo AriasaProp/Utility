c60f5dded994ac7cfb11b47a5741a571a9a95803
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-02 16:34:16 +0800

    implements_template  16:34:16 09/02/24

--- codec_util/main/codec_util.hpp
+++ codec_util/main/codec_util.hpp
@@ -18,9 +18,13 @@ struct codec_data {
 
     template <typename T>
     reader &operator>> (T &);
+    template <>
     reader &operator>><unsigned int> (unsigned int &);
+    template <>
     reader &operator>><unsigned long> (unsigned long &);
+    template <>
     reader &operator>><char> (char &);
+    template <>
     reader &operator>><bool> (bool &);
 
   private:
@@ -33,10 +37,14 @@ struct codec_data {
 
   template <typename T>
   codec_data &operator<< (T);
-  codec_data &operator<< <unsigned int> (unsigned int);
-  codec_data &operator<< <unsigned long> (unsigned long);
-  codec_data &operator<< <char> (char);
-  codec_data &operator<< <bool> (bool);
+  template <>
+  codec_data &operator<<<unsigned int> (unsigned int);
+  template <>
+  codec_data &operator<<<unsigned long> (unsigned long);
+  template <>
+  codec_data &operator<<<char> (char);
+  template <>
+  codec_data &operator<<<bool> (bool);
 
   friend bool operator== (const codec_data &, const codec_data &);
   friend std::ostream &operator<< (std::ostream &, const codec_data &);
