e4a0cecf28d3169759cf0c3b2fc2062428c284ee
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-06 18:20:16 +0800

    use256  18:20:16 09/06/24

--- codec_util/main/huffman_codec.cpp
+++ codec_util/main/huffman_codec.cpp
@@ -10,10 +10,10 @@
 
 // Node structure for Huffman Tree
 struct Node {
-  virtual unsigned int frequency () const {
+  virtual uint32_t frequency () const {
     return 0;
   }
-  virtual unsigned int type () const {
+  virtual uint32_t type () const {
     return 0;
   }
   // Comparator for priority queue
@@ -28,10 +28,10 @@ struct Branch : public Node {
       *right;
   Branch (Node *l, Node *r) : left (l), right (r) {}
 
-  unsigned int frequency () const override {
+  uint32_t frequency () const override {
     return left->frequency () + right->frequency ();
   }
-  unsigned int type () const override {
+  uint32_t type () const override {
     return 2;
   }
   ~Branch () {
@@ -40,19 +40,20 @@ struct Branch : public Node {
   }
 };
 struct Leaf : public Node {
-  uint32_t data, freq;
-  Leaf (uint32_t d, uint32_t f) : data (d), freq (f) {}
+  uint8_t data;
+  uint32_t freq;
+  Leaf (uint8_t d, uint32_t f) : data (d), freq (f) {}
 
-  unsigned int frequency () const override {
+  uint32_t frequency () const override {
     return freq;
   }
-  unsigned int type () const override {
+  uint32_t type () const override {
     return 1;
   }
 };
 
 // Utility function to build Huffman Tree and store the codes
-void buildHuffmanTree (Node *root, std::vector<bool> code, std::unordered_map<uint32_t, std::vector<bool>> &huffmanCode) {
+void buildHuffmanTree (Node *root, std::vector<bool> code, std::unordered_map<uint8_t, std::vector<bool>> &huffmanCode) {
   switch (root->type ()) {
   case 1:
     // Leaf node
@@ -73,13 +74,13 @@ void buildHuffmanTree (Node *root, std::vector<bool> code, std::unordered_map<ui
 
 // Function to encode data using Huffman coding
 const codec_data huffman_encode (codec_data const &cd) {
-  uint32_t temp;
+  uint8_t temp;
   // Encode Huffman codes
   codec_data out_c;
 
   // store frequency each data
   // store 32bit as key, and 16bit for at least 65535 copy
-  std::unordered_map<uint32_t, uint32_t> freq;
+  std::unordered_map<uint8_t, uint32_t> freq;
   // Count frequency of each character
   size_t data_len = 0;
   for (codec_data::reader ro = cd.begin_read (); ro.left ();) {
@@ -88,14 +89,12 @@ const codec_data huffman_encode (codec_data const &cd) {
     ++data_len;
   }
   // write actual 32bit data size and variations of key frequecy
-  out_c << data_len;
-  out_c << size_t (freq.size ());
+  out_c << data_len << size_t (freq.size ());
   // Create priority queue to store live nodes of Huffman tree
   std::priority_queue<Node *, std::vector<Node *>, Node::compare> pq;
-  for (std::pair<uint32_t, uint32_t> pair : freq) {
+  for (std::pair<uint8_t, uint32_t> pair : freq) {
     // Write huffman tree
-    out_c << pair.first;
-    out_c << pair.second;
+    out_c << pair.first << pair.second;
     // Create leaf nodes for each character and add it to the priority queue
     pq.push (new Leaf (pair.first, pair.second));
   }
@@ -108,7 +107,7 @@ const codec_data huffman_encode (codec_data const &cd) {
     pq.push (new Branch (left, right));
   }
   // Traverse the Huffman tree and store Huffman codes in a map
-  std::unordered_map<uint32_t, std::vector<bool>> huffmanCode;
+  std::unordered_map<uint8_t, std::vector<bool>> huffmanCode;
   buildHuffmanTree (pq.top (), std::vector<bool> (), huffmanCode);
   delete pq.top ();
 
@@ -124,15 +123,13 @@ const codec_data huffman_encode (codec_data const &cd) {
 const codec_data huffman_decode (codec_data const &cd) {
   codec_data::reader ro = cd.begin_read ();
   size_t len_data, variations;
-  ro >> len_data;
-  ro >> variations;
-  uint32_t key;
+  ro >> len_data >> variations;
+  uint8_t key;
   uint32_t key_len;
   // Create priority queue to store live nodes of Huffman tree
   std::priority_queue<Node *, std::vector<Node *>, Node::compare> pq;
   for (unsigned i = 0; i < variations; ++i) {
-    ro >> key;
-    ro >> key_len;
+    ro >> key >> key_len;
     pq.push (new Leaf (key, key_len));
   }
   // Create Huffman tree
@@ -153,8 +150,8 @@ const codec_data huffman_decode (codec_data const &cd) {
       ro >> bit_read;
       Node *cur_ = bit_read ? current_branch->right : current_branch->left;
       if (cur_->type () == 1) {
-        out_c << ((Leaf *)cur_)->data;
-        break;
+    		out_c << ((Leaf *)cur_)->data;
+      	break;
       }
       current_branch = (Branch *)cur_;
     }
