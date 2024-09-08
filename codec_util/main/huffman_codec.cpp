38921c52ce066e9592bb3ad2220f8b8baad7357e
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-08 08:45:49 +0800

    use_eof : 08:45:49 09/08/24

--- codec_util/main/huffman_codec.cpp
+++ codec_util/main/huffman_codec.cpp
@@ -51,10 +51,20 @@ struct Leaf : public Node {
     return 1;
   }
 };
+struct EOF : public Node {
+  uint32_t frequency () const override {
+    return 0;
+  }
+  uint32_t type () const override {
+    return 0;
+  }
+};
 
 // Utility function to build Huffman Tree and store the codes
-void buildHuffmanTree (Node *root, std::vector<bool> code, std::unordered_map<uint8_t, std::vector<bool>> &huffmanCode) {
+void buildHuffmanTree (Node *root, std::vector<bool> code, std::unordered_map<uint8_t, std::vector<bool>> &huffmanCode, std::vector<bool> &eof_code) {
   switch (root->type ()) {
+  case 0:
+    break;
   case 1:
     // Leaf node
     huffmanCode[((Leaf *)root)->data] = code;
@@ -82,31 +92,21 @@ const codec_data huffman_encode (codec_data const &cd) {
   // store 32bit as key, and 16bit for at least 65535 copy
   std::unordered_map<uint8_t, uint32_t> freq;
   // Count frequency of each character
-  size_t data_len = 0;
   for (codec_data::reader ro = cd.begin_read (); ro.left ();) {
     ro >> temp;
     ++freq[temp];
-    ++data_len;
   }
   // write actual 32bit data size and variations of key frequecy
-  out_c << data_len << size_t (freq.size ());
+  out_c << size_t (freq.size ());
   // Create priority queue to store live nodes of Huffman tree
   std::priority_queue<Node *, std::vector<Node *>, Node::compare> pq;
 
-  // std::cout << "Keys \n";
-
   for (std::pair<uint8_t, uint32_t> pair : freq) {
     // Write huffman tree
-    out_c << pair.first << pair.second;
-    // std::cout << std::setw (2) << std::setfill ('0') << std::hex << int (pair.first);
-    // std::cout << std::dec << "[" << std::setw (3) << std::setfill ('0') << pair.second << "]  ";
     //  Create leaf nodes for each character and add it to the priority queue
     pq.push (new Leaf (pair.first, pair.second));
   }
-  // std::cout << std::endl;
-
-  // std::cout << "Codec \n";
-  // std::cout << out_c << std::endl;
+  pq.push(new EOF());
   //  Create Huffman tree
   while (pq.size () > 1) {
     Node *left = pq.top ();
@@ -116,8 +116,9 @@ const codec_data huffman_encode (codec_data const &cd) {
     pq.push (new Branch (left, right));
   }
   // Traverse the Huffman tree and store Huffman codes in a map
+  std::vector<bool> eof_code;
   std::unordered_map<uint8_t, std::vector<bool>> huffmanCode;
-  buildHuffmanTree (pq.top (), std::vector<bool> (), huffmanCode);
+  buildHuffmanTree (pq.top (), std::vector<bool> (), huffmanCode, eof_code);
   delete pq.top ();
 
   // Encode input data using Huffman codes
@@ -126,13 +127,15 @@ const codec_data huffman_encode (codec_data const &cd) {
     for (bool s : huffmanCode[temp])
       out_c << s;
   }
+  for (bool s : eof_code)
+    out_c << s;
   return out_c;
 }
 
 const codec_data huffman_decode (codec_data const &cd) {
   codec_data::reader ro = cd.begin_read ();
-  size_t len_data, variations;
-  ro >> len_data >> variations;
+  size_t variations;
+  ro >> variations;
   uint8_t key;
   uint32_t key_len;
 
@@ -142,7 +145,8 @@ const codec_data huffman_decode (codec_data const &cd) {
     ro >> key >> key_len;
     pq.push (new Leaf (key, key_len));
   }
-  std::cout << std::endl;
+  pq.push(new EOF());
+  
   // Create Huffman tree
   while (pq.size () > 1) {
     Node *left = pq.top ();
@@ -154,19 +158,25 @@ const codec_data huffman_decode (codec_data const &cd) {
   Branch *tree = (Branch *)pq.top ();
   codec_data out_c;
   // decode input data using Huffman codes
-  bool bit_read;
-  for (unsigned i = 0; i < len_data; ++i) {
-    Branch *current_branch = tree;
-    while (ro.left ()) {
-      ro >> bit_read;
-      Node *cur_ = bit_read ? current_branch->right : current_branch->left;
-      if (cur_->type () == 1) {
-        out_c << ((Leaf *)cur_)->data;
-        break;
-      }
-      current_branch = (Branch *)cur_;
+  bool bit_read, bool eof_c = false;
+  Branch *current_branch = tree;
+  Node *cur_;
+  do {
+    ro >> bit_read;
+    cur_ = bit_read ? current_branch->right : current_branch->left;
+    switch (cur_->type ()) {
+    	case 0:
+    		eof_c = true;
+    		break;
+    	case 1:
+      	out_c << ((Leaf *)cur_)->data;
+      	current_branch = tree;
+    		break;
+    	case 2:
+      	current_branch = (Branch *)cur_;
+      	break;
     }
-  }
+  } while (!eof_c);
   delete tree;
   return out_c;
 }
