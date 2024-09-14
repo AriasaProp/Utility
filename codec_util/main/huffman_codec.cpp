d0baba9430b24abbe6eef8acb0e302eba1541267
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-14 09:27:18 +0000

    changes of formating

--- codec_util/main/huffman_codec.cpp
+++ codec_util/main/huffman_codec.cpp
@@ -18,7 +18,7 @@ struct Node {
   virtual dat_len frequency () const {
     return 0;
   }
-  virtual dat_len type () const { return -1;}
+  virtual dat_len type () const { return -1; }
   // Comparator for priority queue
   struct compare {
     bool operator() (Node *l, Node *r) {
@@ -105,7 +105,7 @@ const codec_data huffman_encode (codec_data const &cd) {
     ro >> temp;
     ++freq[temp];
   }
-  aVar = freq.size();
+  aVar = freq.size ();
   // Encode Huffman codes
   codec_data out_c;
   // Create priority queue to store live nodes of Huffman tree
@@ -143,7 +143,7 @@ const codec_data huffman_encode (codec_data const &cd) {
 
 namespace decode {
 struct Node {
-  virtual dat_len type () const { return -1;}
+  virtual dat_len type () const { return -1; }
 };
 struct Branch : public Node {
   Node *left, *right;
@@ -176,7 +176,7 @@ Node *readHuffmanTree (codec_data::reader &ro, unsigned char type) {
     ro >> key;
     --aVar;
     if (aVar < 0)
-    	std::cout << "Overload" << std::endl;
+      std::cout << "Overload" << std::endl;
     return new Leaf (key);
   }
   case 2: {
@@ -202,7 +202,7 @@ const codec_data huffman_decode (codec_data const &cd) {
   decode::Branch *current_branch = tree;
   do {
     ro >> bit_read;
-  	decode::Node *cur_ = bit_read ? current_branch->right : current_branch->left;
+    decode::Node *cur_ = bit_read ? current_branch->right : current_branch->left;
     switch (cur_->type ()) {
     default:
     case 0:
