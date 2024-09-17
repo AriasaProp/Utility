ab068acc55de999d7905952d5931294c470f2d5e
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-17 09:43:40 +0800

    debug. : 09:43:40 09/17/24

--- codec_util/main/codec_util.cpp
+++ codec_util/main/codec_util.cpp
@@ -101,7 +101,7 @@ codec_data::reader &operator>> (codec_data::reader &o, unsigned char &d) {
       d |= (*(dt + 1) << (CHAR_BIT - o.readed_bit));
     }
     ++o.readed_byte;
-    std::cout << "Read: " << int ((*dt >> o.readed_bit) | (o.readed_bit ? *(dt + 1) << (CHAR_BIT - o.readed_bit) : 0))
+    std::cout << "Read: " << int ((*dt >> o.readed_bit) | (o.readed_bit ? *(dt + 1) << (CHAR_BIT - o.readed_bit) : 0)) << std::endl;
   }
   return o;
 }
@@ -219,7 +219,8 @@ codec_data &operator<< (codec_data &o, unsigned char in) {
   if (o.used_bit)
     *(dt + 1) = in << (CHAR_BIT - o.used_bit);
   ++o.used_byte;
-  std::cout << "Write: " << int ((*dt >> o.used_bit) | (o.used_bit ? *(dt + 1) << (CHAR_BIT - o.used_bit) : 0)) return o;
+  std::cout << "Write: " << int ((*dt >> o.used_bit) | (o.used_bit ? *(dt + 1) << (CHAR_BIT - o.used_bit) : 0)) << std::endl;
+  return o;
 }
 codec_data &operator<< (codec_data &o, long in) {
   o.check_resize (o.used_byte + sizeof (long) + (o.used_bit > 0));
