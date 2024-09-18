ddaf1e741478204899060b63efeaa188a29318d5
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-18 13:42:53 +0800

    try-read-file : 13:42:53 09/18/24

--- math_util/test/math/pi_extraction_test.cpp
+++ math_util/test/math/pi_extraction_test.cpp
@@ -132,60 +132,36 @@ bool pi_extraction_test () {
   std::cout << "|  digits  || digits/sec ||    byte   |\n";
   std::cout << "-------------------------------------------------------" << std::endl;
   for (base_ex *algo : algos) {
-    // FILE *pi_digits_stream = fopen (DATA_DIR, "rb");
-    // if (pi_digits_stream) {
-    try {
-      // char out;
-      // char result;
-      unsigned long long generated = 0;
-      double timed_average[3] = {0.0000001};
-      auto timed = std::chrono::high_resolution_clock::now ();
-      auto whole_timed = std::chrono::high_resolution_clock::now ();
-      do {
-        // get char aproved result
-        // out = std::getc (pi_digits_stream) - '0';
-        // if (out < 0 || out > 9) // if invalid value (not 0~9)
-        //  continue;
-        /* result = */ algo->extract ();
-        ++generated;
-        // check result
-        /*
-        if (result != out)
-          throw std::logic_error (std::string ("Failure result ") + std::to_string (result) + " != " + std::to_string (out));
-        */
-        {
-          // print result profiling
-          auto now = std::chrono::high_resolution_clock::now ();
+      try {
+        unsigned long long generated = 0;
+        double timed_average[3] = { 0.0000001};
+        auto timed = std::chrono::high_resolution_clock::now ();
+        auto whole_timed = std::chrono::high_resolution_clock::now ();
+        do {
+        	algo->extract ();
+          ++generated;
+        } while (wt < TIME);
+      } catch (const std::exception &e) {
+        std::cout << "\nError: " << e.what () << std::endl;
+      	passed &= false;
+      }
+               {
+            // print result profiling
+            auto now = std::chrono::high_resolution_clock::now ();
 
-          double t_av = (timed_average[0] = timed_average[1]);
-          t_av += (timed_average[1] = timed_average[2]);
-          t_av += (timed_average[2] = std::chrono::duration<double> (now - timed).count ());
-          t_av = 3 / t_av;
-          timed = now;
-          double wt = std::chrono::duration<double> (now - whole_timed).count ();
+            double t_av = (timed_average[0] = timed_average[1]);
+            t_av += (timed_average[1] = timed_average[2]);
+            t_av += (timed_average[2] = std::chrono::duration<double> (now - timed).count ());
+            t_av = 3 / t_av;
+            timed = now;
+            double wt = std::chrono::duration<double> (now - whole_timed).count ();
 
-          std::cout << std::flush << "\r| ";
-          std::cout << std::setfill ('0') << std::setw (8) << generated << " || ";
-          std::cout << std::setfill ('0') << std::setw (10) << std::fixed << std::setprecision (2) << t_av << " || ";
-          std::cout << std::setfill ('0') << std::setw (9) << algo->size () << " |";
+            std::cout << std::flush << "\r| ";
+            std::cout << std::setfill ('0') << std::setw (8) << generated << " || ";
+            std::cout << std::setfill ('0') << std::setw (10) << std::fixed << std::setprecision (2) << t_av << " || ";
+            std::cout << std::setfill ('0') << std::setw (9) << algo->size () << " |";
 
-          if (wt > TIME)
-            throw std::logic_error ("Time out!");
-        }
-      } while (true /*!feof (pi_digits_stream)*/);
-      // check file at the end (first step isn't make sense')
-      // load file with 0 size?
-      throw std::logic_error ("end of file proof");
-    } catch (const std::exception &e) {
-      std::cout << "\nError: " << e.what () << std::endl;
-      passed &= false;
-    }
-    // fclose (pi_digits_stream);
-    /*
-  } else {
-    std::cerr << "Error: failed to open " << o << std::endl;
-  }
-  */
+          }
     delete algo;
   }
   std::cout << "Ended π Test Generator" << std::endl;
