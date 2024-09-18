647ecfd8acfafc106a74dd34ad3dbad94a3d8110
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-09-18 12:07:36 +0000

    changes of formating

--- math_util/test/app_test.cpp
+++ math_util/test/app_test.cpp
@@ -1,18 +1,16 @@
 extern bool BigInteger_test ();
-[[maybe_unused]]
-extern bool matrix_test ();
-[[maybe_unused]]
-extern bool ComplexNumber_test ();
+[[maybe_unused]] extern bool matrix_test ();
+[[maybe_unused]] extern bool ComplexNumber_test ();
 
 extern bool Math_test ();
 
 int main (int argv, char *args[]) {
   bool passed = true;
   passed &= BigInteger_test ();
-/*
-  passed &= matrix_test ();
-  passed &= ComplexNumber_test ();
-*/
+  /*
+    passed &= matrix_test ();
+    passed &= ComplexNumber_test ();
+  */
   passed &= Math_test ();
 
   return !passed;
