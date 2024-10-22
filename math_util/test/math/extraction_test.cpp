5e92564a368313dc5a5cc53c440d23d030c33103
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-22 11:33:42 +0800

    npull : 11:33:42 10/22/24

--- math_util/test/math/extraction_test.cpp
+++ math_util/test/math/extraction_test.cpp
@@ -2,31 +2,29 @@
 //#include "clock_adjustment.hpp"
 
 #include <cerrno>
-#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <deque>
 #include <iomanip>
 #include <iostream>
 #include <memory>
-#include <stdexcept>
-#include <string>
-#include <thread>
+
+
+// Set TIME to 0.0 to reach limit of file digits
+#define TIME 10.0
+#define BUFFER_BYTE_SIZE 4096
 
 // algorithm list
 struct base_ex {
   base_ex () {}
-  virtual char extract () {
-    return -1;
-  }
+  virtual char extract () { return -1; }
+  virtual const char *lbl () = 0;
   virtual const char *testFile () = 0;
   virtual size_t size () {
     return sizeof (base_ex);
   }
   ~base_ex () {}
 };
-
 struct pi_algo : public base_ex {
 private:
   BigInteger q = 1,
@@ -65,6 +63,7 @@ public:
 
     return result;
   }
+  const char *lbl() override { return "π"; }
   const char *testFile () override { return "piDigits.txt"; }
   size_t size () {
     return sizeof (q) + sizeof (r) + sizeof (t) + sizeof (k) + sizeof (l) + sizeof (n);
@@ -72,53 +71,82 @@ public:
   ~pi_algo () {}
 };
 /*
-          1     1     1     1               1
-e => 1 + --- + --- + --- + ---- + ...... + ----
-          1!    2!   3!     4!              n!
-
-          1 > d*100
+     1     1     1x2     1x2x3           n!      
+π = --- + --- + ----- + ------- + ... + -----
+     1    1x3   1x3x5   1x3x5x7          odd!
 */
+struct pis_algo : public base_ex {
+private:
+  BigInteger a = 1, // nominator
+      b = 1,        // denominator
+      
+      c = 1,        // counter
+      d = 3,        // counter odd
+      
+      e = 1,        // factorial
+      
+      g = 1;        // base digit
+
+
+public:
+  pis_algo () {}
+  char extract () override {
+  	while ((d*b) < (e*c*g*1000)) {
+  		e *= c;
+  		a += e;
+  		b *= d;
+  		
+  		d += 2;
+  		++c;
+  	}
+  	char result = char(a / b);
+  	g *= 10;
+  	a *= 10;
+    return result;
+  }
+  const char *lbl() override { return "π"; }
+  const char *testFile () override { return "piDigits.txt"; }
+  size_t size () { return sizeof (a) + sizeof (b) + sizeof (c) + sizeof (d); }
+  ~pis_algo () {}
+};
 struct e_algo : public base_ex {
 private:
   BigInteger a = 2, // nominator
       b = 1,        // denominator
       c = 2,        // counter
-      d = 1,        // base digit
-      e = 0;        // result hold
+      d = 1;        // base digit
 
 public:
   e_algo () {}
   char extract () override {
-    while ((d * d * 1000) > (b * c)) {
-      a *= c;
-      a += d;
-      b *= c;
-      ++c;
-    }
-    e = a / b;
-    char result = static_cast<char> ((int)e);
-    a %= b;
-    d *= 10;
-    a *= 10;
+  	while ((d * d * 1000) > (b * c)) {
+  		a *= c;
+  		a += d;
+  		b *= c;
+  		++c;
+  	}
+  	char result = char(a / b)
+  	a %= b;
+  	d *= 10;
+  	a *= 10;
     return result;
   }
+  const char *lbl() override { return "e"; }
   const char *testFile () override { return "eDigits.txt"; }
-  size_t size () { return sizeof (a) + sizeof (b) + sizeof (c) + sizeof (d) + sizeof (e); }
+  size_t size () { return sizeof (a) + sizeof (b) + sizeof (c) + sizeof (d); }
   ~e_algo () {}
 };
-
-#define TIME 10.0
-#define BUFFER_BYTE_SIZE 4096
 
 bool extraction_test (const char *d) {
   std::cout << "Start Extraction Test Generator" << std::endl;
   bool passed = true;
   base_ex *algos[]{
       new pi_algo (),
+      new pis_algo (),
       new e_algo ()};
   // Draw table header
-  std::cout << "|    digits    || rate(digits/sec) ||   memory(byte)   |\n";
-  std::cout << "|--------------||------------------||------------------|\n";
+  std::cout << " |    digits    || rate(digits/sec) ||   memory(byte)   |\n";
+  std::cout << "-|--------------||------------------||------------------|\n";
 
   // pi proof
   char buff[BUFFER_BYTE_SIZE];
@@ -129,11 +157,11 @@ bool extraction_test (const char *d) {
   long double elapsed_time;
   std::chrono::time_point<std::chrono::high_resolution_clock> start_timed, now_timed;
   for (base_ex *algo : algos) {
-    std::cout << "| ";
+    std::cout << algo->lbl() << "| ";
     try {
       sprintf (buff, "%s/%s", d, algo->testFile ());
       FILE *fpi = fopen (buff, "r");
-      if (!fpi) throw std::logic_error ("file not found");
+      if (!fpi) throw "file not found";
       piIndex = 0;
       piReaded = 0;
       start_timed = std::chrono::high_resolution_clock::now ();
@@ -142,30 +170,30 @@ bool extraction_test (const char *d) {
           piIndex = 0;
           piReaded = fread (buff, 1, BUFFER_BYTE_SIZE, fpi);
         }
-        if (piReaded == 0) {
-          if (feof (fpi))
-            throw std::logic_error ("end of file");
-          if (ferror (fpi))
-            throw std::logic_error (strerror (errno));
-          throw std::logic_error ("cannot get digits from file");
+        if (!piReaded) {
+          if (feof (fpi)) throw "end of file";
+          if (ferror (fpi)) throw strerror (errno);
+          throw "cannot get digits from file";
         }
         result = algo->extract () + '0';
-        if (result != buff[piIndex++]) {
-          throw std::logic_error ("wrong result");
-        }
+        if (result != buff[piIndex++]) throw "wrong result";
         ++generated;
+#if TIME == 0.0
+      } while (true);
+      now_timed = std::chrono::high_resolution_clock::now ();
+      elapsed_time = std::chrono::duration<double> (now_timed - start_timed).count ();
+#else
         now_timed = std::chrono::high_resolution_clock::now ();
         elapsed_time = std::chrono::duration<double> (now_timed - start_timed).count ();
       } while (elapsed_time < TIME);
-      {
-        // print result profiling
-        std::cout << std::setfill ('0') << std::setw (12) << generated << " || ";
-        std::cout << std::setfill ('0') << std::setw (16) << std::fixed << std::setprecision (5) << ((long double)generated / elapsed_time) << " || ";
-        std::cout << std::setfill ('0') << std::setw (16) << algo->size ();
-      }
+#endif
+      // print result profiling
+      std::cout << std::setfill ('0') << std::setw (12) << generated << " || ";
+      std::cout << std::setfill ('0') << std::setw (16) << std::fixed << std::setprecision (5) << ((long double)generated / elapsed_time) << " || ";
+      std::cout << std::setfill ('0') << std::setw (16) << algo->size ();
       fclose (fpi);
-    } catch (const std::exception &e) {
-      std::cout << std::setfill (' ') << std::setw (43) << std::internal << "Error: " << e.what ();
+    } catch (const char *e) {
+      std::cout << std::setfill (' ') << std::setw (43) << std::internal << "Error: " << e;
       passed &= false;
     }
     std::cout << " |" << std::endl;
