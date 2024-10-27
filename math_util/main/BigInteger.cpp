96a3d63ff4a8bc5b701cbdec90ddb39092cc2ea5
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-10-27 16:27:58 +0800

    pi : 16:27:58 10/27/24

--- math_util/main/BigInteger.cpp
+++ math_util/main/BigInteger.cpp
@@ -528,8 +528,7 @@ BigInteger &BigInteger::operator*= (const BigInteger b) {
   return *this;
 }
 BigInteger &BigInteger::operator/= (const signed b) {
-  if (b == 0)
-    throw ("Undefined number cause / 0 !");
+  if (!b) throw ("Undefined number cause / 0 !");
   std::vector<word> rem = words, div{word (abs (b))};
   words.clear ();
   if (compare (rem, div) >= 0) {
@@ -934,14 +933,15 @@ BigInteger BigInteger::operator- (const BigInteger b) const {
 BigInteger BigInteger::operator* (const signed b) const {
   BigInteger result;
   if (b && words.size ()) {
-    result.neg = neg ^ b < 0;
+    result.neg = neg ^ (b < 0);
     std::vector<word> &r = result.words;
+    r.resize(words.size());
     r.reserve (words.size () + 1);
     const word B = word (abs (b));
     const word b_hi = B >> WORD_HALF_BITS;
     const word b_lo = B & WORD_HALF_MASK;
     word a_hi, a_lo, carry = 0, carry0;
-    for (size_t i = 0; i < words.size (); ++i) {
+    for (size_t i = 0; i < words.size(); ++i) {
       const word &wA = words[i];
       a_hi = wA >> WORD_HALF_BITS;
       a_lo = wA & WORD_HALF_MASK;
