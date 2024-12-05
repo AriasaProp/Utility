ed3e1329f08e65557b0fe9e43be13952f5fbf86c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-05 11:51:08 +0000

    changes of formating

--- codec_util/test/app_test.cpp
+++ codec_util/test/app_test.cpp
@@ -13,24 +13,23 @@
 int main (int argv, char *args[]) {
   std::cout << "Start Codec Test" << std::endl;
   try {
-  	const char *files[]{
-  		"01.png",
-  		"02.png",
-  		"03.png",
-  	};
-  	int outx, outy, outc;
-  	unsigned char *s;
-  	char buff[1024];
-  	for (int f = 1; f < 4; ++f) {
+    const char *files[]{
+        "01.png",
+        "02.png",
+        "03.png",
+    };
+    int outx, outy, outc;
+    unsigned char *s;
+    char buff[1024];
+    for (int f = 1; f < 4; ++f) {
       sprintf (buff, "%e/%02d.png", argv[1], f);
-  		std::cout << "Loading: " << buff << " is ";
-  		s = stbi::load::load_from_filename(buff, &outx, &outy, &outc, 0);
-  		if (!s) throw stbi::load::failure_reason();
-  		std::cout << " Succes " << std::endl;
-  		
-  		
-  		stbi::load::image_free(s);
-  	}
+      std::cout << "Loading: " << buff << " is ";
+      s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
+      if (!s) throw stbi::load::failure_reason ();
+      std::cout << " Succes " << std::endl;
+
+      stbi::load::image_free (s);
+    }
   } catch (const char *err) {
     std::cout << "Error : " << err << std::endl;
     return EXIT_FAILURE;
