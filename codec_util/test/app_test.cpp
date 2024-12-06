561599e1cf0f3d5718b2f9807e60eb7e8db1ce03
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-06 13:54:01 +0000

    changes of formating

--- codec_util/test/app_test.cpp
+++ codec_util/test/app_test.cpp
@@ -1,13 +1,13 @@
-#include "stbi/stbi_load.hpp"
 #include "image_codec.hpp"
+#include "stbi/stbi_load.hpp"
 
 #include <chrono>
 #include <cstdint>
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <map>
 #include <random>
-#include <cstring>
 #include <string>
 #include <utility>
 #include <vector>
@@ -25,21 +25,21 @@ int main (int argv, char *args[]) {
       s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
       outbytes = outx * outy * outc;
       if (!s) throw stbi::load::failure_reason ();
-      image_param img_p {(unsigned int)outx, (unsigned int)outy, (unsigned char)outc};
-      ic = image_encode(s, img_p, &outbytes);
-      is = image_decode(ic, outbytes, &img_p);
+      image_param img_p{(unsigned int)outx, (unsigned int)outy, (unsigned char)outc};
+      ic = image_encode (s, img_p, &outbytes);
+      is = image_decode (ic, outbytes, &img_p);
 
       stbi::load::image_free (s);
-      
-      if (memcmp(s, is, outx * outy * outc)) {
-      	std::cout << " Failure ";
+
+      if (memcmp (s, is, outx * outy * outc)) {
+        std::cout << " Failure ";
       } else {
-      	std::cout << " Succes with " << outbytes << " bytes";
+        std::cout << " Succes with " << outbytes << " bytes";
       }
       std::cout << std::endl;
-      
-      image_free(ic);
-      image_free(is);
+
+      image_free (ic);
+      image_free (is);
     }
   } catch (const char *err) {
     std::cout << "Error : " << err << std::endl;
