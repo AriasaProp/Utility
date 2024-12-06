cf5e95284380b4031013f157ac8d6b1da139febe
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-06 20:59:23 +0800

    test : 20:59:23 12/06/24

--- codec_util/test/app_test.cpp
+++ codec_util/test/app_test.cpp
@@ -1,5 +1,5 @@
-#include "image_codec.hpp"
 #include "stbi/stbi_load.hpp"
+#include "image_codec.hpp"
 
 #include <chrono>
 #include <cstdint>
@@ -15,30 +15,29 @@ int main (int argv, char *args[]) {
   std::cout << "Start Codec Test" << std::endl;
   try {
     int outx, outy, outc, outbytes;
-    unsigned char *s, ic, is;
+    unsigned char *s, *ic, *is;
     char buff[1024];
     for (unsigned int f = 1; f < 4; ++f) {
       sprintf (buff, "%s/%02d.png", args[1], f);
-      std::cout << "Test: " << buff;
-      << " comp ";
+      std::cout << "Test: " << buff; << " comp ";
       s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
       outbytes = outx * outy * outc;
       if (!s) throw stbi::load::failure_reason ();
-      image_param img_p{outx, outy, outc};
-      ic = image_encode (s, img_p, &outbytes);
-      is = image_encode (ic, outbytes, &img_p);
+      image_param img_p {outx, outy, outc};
+      ic = image_encode(s, img_p, &outbytes);
+      is = image_decode(ic, outbytes, &img_p);
 
       stbi::load::image_free (s);
-
-      if (memcmp (s, is, outx * outy * outc)) {
-        std::cout << " Failure ";
+      
+      if (memcmp(s, is, outx * outy * outc)) {
+      	std::cout << " Failure ";
       } else {
-        std::cout << " Succes with " << outbytes << " bytes";
+      	std::cout << " Succes with " << outbytes << " bytes";
       }
       std::cout << std::endl;
-
-      image_free (ic);
-      image_free (is);
+      
+      image_free(ic);
+      image_free(is);
     }
   } catch (const char *err) {
     std::cout << "Error : " << err << std::endl;
