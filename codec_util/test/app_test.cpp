bcb8b50b0b8b501d00dc034570080efa8e29e943
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-06 19:38:04 +0800

    test : 19:38:04 12/06/24

--- codec_util/test/app_test.cpp
+++ codec_util/test/app_test.cpp
@@ -1,4 +1,5 @@
 #include "stbi/stbi_load.hpp"
+#include "image_codec.hpp"
 
 #include <chrono>
 #include <cstdint>
@@ -13,22 +14,30 @@
 int main (int argv, char *args[]) {
   std::cout << "Start Codec Test" << std::endl;
   try {
-    const char *files[]{
-        "01.png",
-        "02.png",
-        "03.png",
-    };
-    int outx, outy, outc;
-    unsigned char *s;
+    int outx, outy, outc, outbytes;
+    unsigned char *s, ic, is;
     char buff[1024];
     for (unsigned int f = 1; f < 4; ++f) {
       sprintf (buff, "%s/%02d.png", args[1], f);
-      std::cout << "Loading: " << buff << " is ";
+      std::cout << "Test: " << buff; << " comp ";
       s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
+      outbytes = outx * outy * outc;
       if (!s) throw stbi::load::failure_reason ();
-      std::cout << " Succes " << std::endl;
+      image_param img_p {outx, outy, outc};
+      ic = image_encode(s, img_p, &outbytes);
+      is = image_encode(ic, outbytes, &img_p);
 
       stbi::load::image_free (s);
+      
+      if (memcmp(s, is, outx * outy * outc)) {
+      	std::cout << " Failure ";
+      } else {
+      	std::cout << " Succes with " << outbytes << " bytes";
+      }
+      std::cout << std::endl;
+      
+      image_free(ic);
+      image_free(is);
     }
   } catch (const char *err) {
     std::cout << "Error : " << err << std::endl;
@@ -37,6 +46,5 @@ int main (int argv, char *args[]) {
     std::cout << "Error : uncaught" << std::endl;
     return EXIT_FAILURE;
   }
-
   return EXIT_SUCCESS;
 }
