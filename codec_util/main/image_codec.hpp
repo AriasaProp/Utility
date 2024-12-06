0c1c1e922c0142a71aae74596724082dfd9de9e4
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-06 11:38:25 +0000

    changes of formating

--- codec_util/main/image_codec.hpp
+++ codec_util/main/image_codec.hpp
@@ -1,17 +1,17 @@
 #ifndef IMAGE_CODEC_
 #define IMAGE_CODEC_
 
-// width pixel, height pixel, channel per pixels ? 3 or 4 
+// width pixel, height pixel, channel per pixels ? 3 or 4
 struct image_param {
-	unsigned int width, height;
-	unsigned char channel;
+  unsigned int width, height;
+  unsigned char channel;
 };
 
 // input: data, image param in, out len in bytes
-unsigned char *image_encode(const unsigned char *, const image_param, unsigned int*);
-// input: data, data length in byte, image params out 
-unsigned char *image_decode(const unsigned char *, const unsigned int, image_param*);
-//void release memory
-void image_free(unsigned char *);
+unsigned char *image_encode (const unsigned char *, const image_param, unsigned int *);
+// input: data, data length in byte, image params out
+unsigned char *image_decode (const unsigned char *, const unsigned int, image_param *);
+// void release memory
+void image_free (unsigned char *);
 
 #endif // IMAGE_CODEC_
--- codec_util/test/app_test.cpp
+++ codec_util/test/app_test.cpp
@@ -1,5 +1,5 @@
-#include "stbi/stbi_load.hpp"
 #include "image_codec.hpp"
+#include "stbi/stbi_load.hpp"
 
 #include <chrono>
 #include <cstdint>
@@ -19,25 +19,26 @@ int main (int argv, char *args[]) {
     char buff[1024];
     for (unsigned int f = 1; f < 4; ++f) {
       sprintf (buff, "%s/%02d.png", args[1], f);
-      std::cout << "Test: " << buff; << " comp ";
+      std::cout << "Test: " << buff;
+      << " comp ";
       s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
       outbytes = outx * outy * outc;
       if (!s) throw stbi::load::failure_reason ();
-      image_param img_p {outx, outy, outc};
-      ic = image_encode(s, img_p, &outbytes);
-      is = image_encode(ic, outbytes, &img_p);
+      image_param img_p{outx, outy, outc};
+      ic = image_encode (s, img_p, &outbytes);
+      is = image_encode (ic, outbytes, &img_p);
 
       stbi::load::image_free (s);
-      
-      if (memcmp(s, is, outx * outy * outc)) {
-      	std::cout << " Failure ";
+
+      if (memcmp (s, is, outx * outy * outc)) {
+        std::cout << " Failure ";
       } else {
-      	std::cout << " Succes with " << outbytes << " bytes";
+        std::cout << " Succes with " << outbytes << " bytes";
       }
       std::cout << std::endl;
-      
-      image_free(ic);
-      image_free(is);
+
+      image_free (ic);
+      image_free (is);
     }
   } catch (const char *err) {
     std::cout << "Error : " << err << std::endl;
