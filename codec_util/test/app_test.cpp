2e00a048fee95093c3e3525c82438ba81b347642
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-07 19:49:28 +0800

    test : 19:49:28 12/07/24

--- codec_util/test/app_test.cpp
+++ codec_util/test/app_test.cpp
@@ -19,15 +19,13 @@ int main (int argv, char *args[]) {
     unsigned int outbytes;
     unsigned char *s, *ic, *is;
     char buff[1024];
-    for (unsigned int f = 0; f <= /*5*/ 2; ++f) {
+    for (unsigned int f = 0; f <= 5; ++f) {
       sprintf (buff, "%s/%02d.png", args[1], f);
       std::cout << "Test: " << buff;
-      s = new unsigned char[16]{
-          0xab, 0xf0, 0xba, 0x0f, 0xab, 0xff, 0x3a, 0xff, 0x77, 0x6f, 0xba, 0x1f, 0xab, 0xff, 0xba, 0xbf};
-      // s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
+      s = stbi::load::load_from_filename (buff, &outx, &outy, &outc, 0);
       outbytes = outx * outy * outc;
-      // if (!s) throw stbi::load::failure_reason ();
-
+      if (!s) throw stbi::load::failure_reason ();
+      
       image_param img_p{(unsigned int)outx, (unsigned int)outy, (unsigned char)outc};
       ic = image_encode (s, img_p, &outbytes);
       is = image_decode (ic, outbytes, &img_p);
@@ -36,16 +34,15 @@ int main (int argv, char *args[]) {
         std::cout << " Failure " << std::endl;
         std::cout << "A info x " << outx << " y " << outy << " ch " << outc << std::endl;
         std::cout << "B info x " << img_p.width << " y " << img_p.height << " ch " << (int)img_p.channel << std::endl;
-        int *a = reinterpret_cast<int *> (s), *b = reinterpret_cast<int *> (is);
-        for (unsigned int i = 0; i < outx * outy; ++i)
-          std::cout << std::hex << *(a + i) << " : " << std::hex << *(b + i) << std::endl;
+      	int *a = reinterpret_cast<int*>(s), *b = reinterpret_cast<int*>(is);
+      	for (unsigned int i = 0; i < outx * outy; ++i)
+    			std::cout << std::hex << *(a+i) << " : " << std::hex << *(b+i) << std::endl;
       } else {
         std::cout << " Succes with " << outbytes << " bytes";
       }
       std::cout << std::endl;
 
-      // stbi::load::image_free (s);
-      delete[] s;
+      stbi::load::image_free (s);
       image_free (ic);
       image_free (is);
     }
