cd09eec097ace0a2e54904d3a0c04a2b014cfca8
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2024-12-10 12:00:19 +0800

    has : 12:00:18 12/10/24

--- codec_util/main/image_codec.cpp
+++ codec_util/main/image_codec.cpp
@@ -22,12 +22,12 @@
 const unsigned int HEADER_SIZE = 8;
 const unsigned char HEADER_ARRAY[]{0x49, 0x4d, 0x47, 0x43, 0x4f, 0x44, 0x45, 0x43};
 
-unsigned char hashing (unsigned char *in, unsigned char len) {
-  unsigned char r;
-  for (unsigned char i = len * 2 + 1; i < len; i -= 2) {
-    r += *(in++) * i;
-  }
-  return r & IMGC_MASKVALUE;
+unsigned char hashing (const unsigned char *in, unsigned char len) {
+	unsigned char r;
+	for (unsigned char i = len * 2 + 1; i < len; i -= 2 ) {
+		r += *(in++) * i;
+	}
+	return r & IMGC_MASKVALUE;
 }
 
 // input: data, width pixel, height pixel, channel per pixels ? 3 or 4
@@ -61,19 +61,17 @@ unsigned char *image_encode (const unsigned char *pixels, const image_param para
       if (px_cmp < 65) { // there is lookup
         write_px.push_back (IMGC_LOOKBACK | (px_cmp - 1));
       } else {
-        hash_ = hashing (read_px, param.channel);
-        if (!memcmp (hash_px + (hash_ * param.channel), read_px, param.channel)) {
-          // write hash index
-          write_px.push_back (IMGC_HASHINDEX | hash_);
-        } else {
-
-          // write full channel
-          write_px.push_back (IMGC_FULLCHANNEL);
-          write_px.insert (write_px.end (), read_px, read_px + param.channel);
-
-          // put new pixel to indexing hash
-          memcpy (hash_px + (hash_ * param.channel), read_px, param.channel);
-        }
+      	hash_ = hashing(read_px, param.channel);
+      	if (!memcmp(hash_px + (hash_ * param.channel), read_px, param.channel)) {
+      		// write hash index
+	        write_px.push_back (IMGC_HASHINDEX | hash_);
+      	} else {
+	        // write full channel
+	        write_px.push_back (IMGC_FULLCHANNEL);
+	        write_px.insert (write_px.end (), read_px, read_px + param.channel);
+	        // put new pixel to indexing hash
+	        memcpy(hash_px + (hash_ * param.channel), read_px, param.channel);
+      	}
       }
     } else if ((++run > 63) || (read_px + param.channel >= end_px)) {
       write_px.push_back (IMGC_RUNLENGTH | (run - 1));
@@ -138,8 +136,8 @@ unsigned char *image_decode (const unsigned char *bytes, const unsigned int byte
       switch (readed) {
       case IMGC_FULLCHANNEL:
         memcpy (write_px, read_px, param->channel);
-        hash_ = hashing (read_px, param->channel);
-        memcpy (hash_px + (hash_ * param.channel), read_px, param->channel);
+        hash_ = hashing(read_px, param->channel);
+        memcpy (hash_px + (hash_ * param->channel), read_px, param->channel);
         write_px += param->channel;
         read_px += param->channel;
         break;
