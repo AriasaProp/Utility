386a9742b750597a81fdeb64177115865ac31d03
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-07 17:50:56 +0800

    newtest : 17:50:56 01/07/25

--- profiling_util/main/simple_clock.hpp
+++ profiling_util/main/simple_clock.hpp
@@ -4,7 +4,11 @@
 #include <ctime>
 #include <iostream>
 
-std::ostream &operator<< (std::ostream &, const std::clock_t &);
+struct simple_time_t {
+	std::clock_t t;
+	
+	friend std::ostream &operator<< (std::ostream &, const simple_time_t &);
+}
 
 struct simple_timer_t {
 private:
@@ -12,7 +16,7 @@ private:
 
 public:
   void start ();
-  std::clock_t end ();
+  simple_time_t end ();
 };
 
 #endif // SIMPLE_CLOCK_
