d165515dcdbfb4ded552ca80bcb714d1f1a45e4e
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-07 20:10:53 +0800

    newtest : 20:10:53 01/07/25

--- profiling_util/main/simple_clock.cpp
+++ profiling_util/main/simple_clock.cpp
@@ -10,29 +10,23 @@ simple_time_t simple_timer_t::end () {
 }
 
 std::ostream &operator<< (std::ostream &o, const simple_time_t &t) {
-  static const char *unit[]{"m", "s", "ms", "ns", "us"};
-  unsigned int arr[5];
-
-  long double td = t.t * 1.0 / CLOCKS_PER_SEC;
-  arr[0] = static_cast<unsigned int> (td / 60.0);
-  td -= arr[0] * 60;
-  arr[1] = static_cast<unsigned int> (td);
-  td -= arr[1];
-  td *= 1000.0;
-  arr[2] = static_cast<unsigned int> (td);
-  td -= arr[2];
-  td *= 1000.0;
-  arr[3] = static_cast<unsigned int> (td);
-  td -= arr[3];
-  td *= 1000.0;
-  arr[4] = static_cast<unsigned int> (td);
-
+  static const char *unit[]{"m", "s", "ms", "us"};
+  double td = t.t * 1.0 / CLOCKS_PER_SEC;
+  bool non = true;
   std::stringstream ss;
-  for (unsigned int i = 0; i < 5; ++i) {
-    if (arr[i])
-      ss << " " << arr[i] << " " << unit[i];
-  }
+  unsigned int t = static_cast<unsigned int> (td / 60.0);
+  if (t) ss << t << " M", non = false;
+  td -= t * 60;
+  t = static_cast<unsigned int> (td);
+  if (t) ss << (non ? "" : ", ") << t << " s", non = false;
+  td -= t;
+  td *= 1000.0;
+  t = static_cast<unsigned int> (td);
+  if (t) ss << (non ? "" : ", ") << t << " ms", non = false;
+  td -= t;
+  td *= 1000.0;
+  t = static_cast<unsigned int> (td);
+  if (t || non) ss << (non ? "" : ", ") << t << " us";
   o << ss.str ();
-
   return o;
 }
