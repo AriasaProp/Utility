06e886913df9479b939f75883ac6e7446f12fd3a
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-08 00:07:51 +0800

    newtest : 00:07:51 01/08/25

--- profiling_util/main/simple_clock.cpp
+++ profiling_util/main/simple_clock.cpp
@@ -1,10 +1,10 @@
 #include "simple_clock.hpp"
 
-#include <iomanip>
 #include <sstream>
+#include <iomanip>
 
-double simple_time_t::to_sec () {
-  return t * 1.0 / CLOCKS_PER_SEC;
+double simple_time_t::to_sec() {
+	return t * 1.0 / CLOCKS_PER_SEC;
 }
 
 void simple_timer_t::start () {
@@ -19,17 +19,24 @@ std::ostream &operator<< (std::ostream &o, const simple_time_t &t) {
   double td = t.t * 1.0 / CLOCKS_PER_SEC;
   unsigned int writed = 0;
   unsigned int r = static_cast<unsigned int> (td);
-  if (r) ss << (writed ? "" : ", ") << r << " s", ++writed;
-  if (writed > 1) return o;
+  if (r) ss << r << " s", ++writed;
+  if (writed > 1) return o; 
   td -= r;
   td *= 1000.0;
   r = static_cast<unsigned int> (td);
-  if (r) ss << (writed ? "" : ", ") << std::setw (3) << std::setfill ('0') << r << " ms", ++writed;
-  if (writed > 1) return o;
+  if (r) {
+  	if (writed) ss << ", ";
+  	ss << std::setw(3) << std::setfill('0') << r << " ms";
+  	++writed;
+  }
+  if (writed > 1) return o; 
   td -= r;
   td *= 1000.0;
   r = static_cast<unsigned int> (td);
-  if (r || (writed == 0)) ss << (writed ? "" : ", ") << std::setw (3) << std::setfill ('0') << r << " us";
+  if (r || !writed) {
+  	if (writed) ss << ", ";;
+  	ss << std::setw(3) << std::setfill('0') << r << " us";
+  }
   o << ss.str ();
   return o;
 }
