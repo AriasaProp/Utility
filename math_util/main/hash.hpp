49df6d416fee137230f4b328ca3730577af4e27c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-14 09:26:17 +0000

    changes of formating

--- math_util/main/hash.hpp
+++ math_util/main/hash.hpp
@@ -5,10 +5,10 @@
 #include <cstdlib>
 
 union hash256 {
-	char b[32];
-	uint32_t i[8];
+  char b[32];
+  uint32_t i[8];
 };
-std::ostream &operator<<(std::ostream &, const hash256);
+std::ostream &operator<< (std::ostream &, const hash256);
 
 hash256 sha256 (const char *, size_t, uint32_t *);
 
