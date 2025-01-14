7e638aaf080471296e65afc63fca234d4cfa2156
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-14 17:32:26 +0800

    mining-test : 17:32:26 01/14/25

--- math_util/main/hash.hpp
+++ math_util/main/hash.hpp
@@ -5,11 +5,11 @@
 #include <cstdlib>
 
 union hash256 {
-  char b[32];
-  uint32_t i[8];
+	char b[32];
+	uint32_t i[8];
 };
-std::ostream &operator<< (std::ostream &, const hash256);
+std::ostream &operator<<(std::ostream &, const hash256);
 
-hash256 sha256 (const char *, size_t, uint32_t *);
+hash256 sha256 (const char *, size_t);
 
 #endif // _HASH_FUNCTION
