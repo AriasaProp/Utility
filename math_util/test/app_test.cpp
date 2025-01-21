36b209bf3a62f1e7c82037a21f81ac03784f5d1c
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-21 10:46:14 +0800

    test : 10:46:14 01/21/25

--- math_util/test/app_test.cpp
+++ math_util/test/app_test.cpp
@@ -29,8 +29,8 @@ int main (int argv, char *args[]) {
       // basic class test
       BigInteger_test () &&
       hash_test (args[1]) &&
-      ComplexNumber_test () &&
-      matrix_test () &&
+      //ComplexNumber_test () &&
+      //matrix_test () &&
       // performance function test
       extraction_test (args[1]) &&
       mining_test ()
