da19b2e15395c616075c1a917270be7335156372
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-01-22 19:00:42 +0800

    test : 19:00:42 01/22/25

--- math_util/test/test/complex_number_test.cpp
+++ math_util/test/test/complex_number_test.cpp
@@ -11,15 +11,18 @@ bool complex_number_test () {
 	*output_file << "complex_number Test: ";
 	simple_timer_t c;
 	try {
-	  if (
-	  	complex_number (5, 7) != complex_number (2, 3) + complex_number (3, 4) ||
-	  	complex_number (-13, 9) != complex_number (-6, 8) - complex_number (7, -1) ||
-	  	complex_number (8, 12) != complex_number (-1, 2) * complex_number (4, -4) ||
-	  	7 != complex_number (7, 14) / complex_number (1, 2) ||
-	  	complex_number (-3, 6) != (-3 + 6 * complex_number (0, 1)) ||
-	  	complex_number (122, -597) != (complex_number (2, 3) ^ 5)
-	  	
-	  ) throw "standar operation wrong!";
+	  if (complex_number (5, 7) != (complex_number (2, 3) + complex_number (3, 4)))
+	  	throw "complex addition wrong";
+	  if (complex_number (-13, 9) != (complex_number (-6, 8) - complex_number (7, -1)))
+	  	throw "complex subtract wrong";
+	  if (complex_number (8, 12) != (complex_number (-1, 2) * complex_number (4, -4)))
+	  	throw "complex multiply wrong";
+	  if (7 != (complex_number (7, 14) / complex_number (1, 2)))
+	  	throw "complex division wrong";
+	  if (complex_number (-3, 6) != (-3 + 6 * complex_number (0, 1)))
+	  	throw "complex operate with number is wrong";
+	  if (complex_number (122, -597) != (complex_number (2, 3) ^ 5))
+	  	throw "complex powerOf wrong";
 	} catch (const char *err) {
 		std::cerr << "Error: " << err << std::endl;
 		return false;
